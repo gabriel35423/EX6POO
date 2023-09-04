9d16b8fd0478855afd36da5556dd1e0e0ce504f4
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:24:34 -0300

    Add files via upload

---
+++ ProgramEx6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listaPOOex6
+{
+    internal class Dolar
+    {
+        private double Reais;
+        private double qnt;
+
+        public void setqnt(double d)
+        {
+            qnt = d;
+        }
+        public double getReais()
+        {
+            return Reais; 
+        }
+        public double getqnt()
+        {
+            return qnt;
+        }
+        public void calcular()
+        {
+            Reais = qnt * 4.97;
+        }
+
+
+    }
+}
